82b5dbc685b625c464e81a79ad12aa0ebc00141a
LACOMBE Dorian <dorian.lacombe@etu.univ-amu.fr>
2025-03-13 12:16:26 +0100

    init premiers fichiers

---
+++ src/components/HeaderComponent/HeaderComponent.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const HeaderComponent = () => (
+     <header>
+         <h1>Todo amU</h1>
+         <p>X Tâches dont Y En cours (N Affichées)</p>
+     </header>
+);
+
+export default HeaderComponent;
